f284cb37b3500ae4be6c421d54313ffd7e207cd0
inessrochaa <ines.f.a.rocha@gmail.com>
2025-03-16 20:05:20 +0000

    Update signalFiltration.m

--- signalFiltration.m
+++ signalFiltration.m
@@ -5,7 +5,7 @@
 % (difference equation in the paper)
 
 function H_filtered = signalFiltration(H)
-    br = [0.020 0 -0.040 0 0.020] % br coefficients (x) (numerator)
-    ak = [1 -3.411 4.497 -2.723 0.639] % ak coefficients (y) (denominator)
+    br = [0.020 0 -0.040 0 0.020]; % br coefficients (x) (numerator)
+    ak = [1 -3.411 4.497 -2.723 0.639]; % ak coefficients (y) (denominator)
     H_filtered = filter(br, ak, H);
 end
